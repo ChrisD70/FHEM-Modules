9f1eecafbfcb82b12f2132f6f6fc7523aca2e4c1
ChrisD70 <chris_dud@gmx.net>
2017-04-28 21:44:01 +0200

    AutoUpdate SB

--- autoupdate/sb/FHEM/98_SB_PLAYER.pm
+++ autoupdate/sb/FHEM/98_SB_PLAYER.pm
@@ -1,5 +1,5 @@
 ﻿# ##############################################################################
-# $Id: 98_SB_PLAYER.pm 0073 2017-04-19 20:41:00Z CD/MM/Matthew/Heppel $
+# $Id: 98_SB_PLAYER.pm 0074 2017-04-28 21:34:00Z CD/MM/Matthew/Heppel $
 #
 #  FHEM Module for Squeezebox Players
 #
@@ -1428,6 +1428,11 @@ sub SB_PLAYER_Parse( $$ ) {
             readingsBulkUpdate( $hash, "presence", "absent" );
             readingsBulkUpdate( $hash, "state", "off" );
             readingsBulkUpdate( $hash, "power", "off" );
+            # CD 0074 bei disconnect playStatus und Timer zurücksetzen
+            readingsBulkUpdate( $hash, "playStatus", "stopped" );
+            RemoveInternalTimer( $hash );
+            RemoveInternalTimer( "QueryElapsedTime:$name");
+            # CD 0074 end
             SB_PLAYER_Amplifier( $hash );
             # CD 0031 wenn Player während TTS verschwindet Zustand zurücksetzen
             if(($hash->{helper}{ttsstate}>TTS_IDLE)&&($hash->{helper}{ttsstate}<TTS_RESTORE)) {
