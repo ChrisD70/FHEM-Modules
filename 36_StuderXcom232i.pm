6c9b782a36886af651c4db47df56fbb3e3be15e5
ChrisD70 <chris_dud@gmx.net>
2017-06-17 18:55:49 +0200

    Test StuderXT

---
+++ 36_StuderXcom232i.pm
@@ -0,0 +1,544 @@
+##############################################
+# $Id: 36_StuderXcom232i.pm 0001 $
+# 150415 0001 initial release
+# 150417 0002 ignore StuderXcom232i_Write calls until init_done
+# 150426 0003 added set XXX test
+# 150507 0004 removed set XXX test, activated dispatch
+# 150515 0005 fixed writing FLOATs 
+# TODO:
+
+package main;
+
+use strict;
+use warnings;
+use Time::HiRes qw(gettimeofday time);
+use Digest::MD5 qw(md5);
+use bytes;
+
+sub StuderXcom232i_Initialize($);
+sub StuderXcom232i_Define($$);
+sub StuderXcom232i_Undef($$);
+sub StuderXcom232i_Attr(@);
+sub StuderXcom232i_Set($@);
+sub StuderXcom232i_ReadAnswer($$$);
+sub StuderXcom232i_Write($$);
+sub StuderXcom232i_Read($);
+sub StuderXcom232i_Parse($$);
+sub StuderXcom232i_Ready($);
+sub StuderXcom232i_SimpleWrite(@);
+sub StuderXcom232i_DoInit($);
+sub StuderXcom232i_Poll($);
+sub StuderXcom232i_AddWQueue($$);
+sub StuderXcom232i_AddRQueue($$);
+sub StuderXcom232i_Timeout($);
+sub StuderXcom232i_HandleWriteQueue($);
+sub StuderXcom232i_HandleReadQueue($);
+sub StuderXcom232i_Reconnect($);
+sub StuderXcom232i_LogFrame($$$$);
+sub StuderXcom232i_checksum_is_ok($);
+sub StuderXcom232i_checksum($);
+
+use constant READ_PROPERTY    => 1;
+use constant WRITE_PROPERTY   => 2;
+
+sub StuderXcom232i_Initialize($) {
+  my ($hash) = @_;
+
+  require "$attr{global}{modpath}/FHEM/DevIo.pm";
+
+# Provider
+  $hash->{ReadFn}  = "StuderXcom232i_Read";
+  $hash->{WriteFn} = "StuderXcom232i_Write";
+  $hash->{ReadyFn} = "StuderXcom232i_Ready";
+  $hash->{SetFn}   = "StuderXcom232i_Set";
+  $hash->{NotifyFn}= "StuderXcom232i_Notify";
+  $hash->{AttrFn}  = "StuderXcom232i_Attr";
+  $hash->{Clients} = ":StuderXT:";
+  my %mc = (
+    "1:StuderXT" => "^StuderXT.*"
+  );
+  $hash->{MatchList} = \%mc;
+
+# Normal devices
+  $hash->{DefFn}   = "StuderXcom232i_Define";
+  $hash->{UndefFn} = "StuderXcom232i_Undef";
+  $hash->{AttrList}= "do_not_notify:1,0 dummy:1,0 " .
+                     "timeout " .
+                     $readingFnAttributes;
+}
+sub StuderXcom232i_Define($$) {#########################################################
+  my ($hash, $def) = @_;
+  my @a = split("[ \t][ \t]*", $def);
+
+  if(@a != 3) {
+    my $msg = "wrong syntax: define <name> StuderXcom232i {devicename[\@baudrate] ".
+                        "| devicename\@directio | hostname:port}";
+    Log3 $hash, 2, $msg;
+    return $msg;
+  }
+ 
+  DevIo_CloseDev($hash);
+
+  my $name = $a[0];
+  my $dev = $a[2];
+  $dev .= "\@38400" if( ($dev !~ m/\@/) && ($dev !~ m/:/));
+  
+  $hash->{DeviceName} = $dev;
+  $hash->{STATE} = "disconnected";
+  $hash->{helper}{databits}=8;
+  $hash->{helper}{parity}='even';
+  $hash->{helper}{stopbits}=1;
+  $hash->{dummy}=0;
+  
+  my $ret;
+  
+  if ($init_done){
+    $ret = DevIo_OpenDev($hash, 0, "StuderXcom232i_DoInit");
+  } else {
+    InternalTimer(gettimeofday()+10, "StuderXcom232i_Reconnect", "reconnect:".($hash->{NAME}), 0);
+  }
+
+  return $ret;
+}
+sub StuderXcom232i_Undef($$) {##########################################################
+  my ($hash, $arg) = @_;
+  my $name = $hash->{NAME};
+
+  foreach my $d (sort keys %defs) {
+    if(defined($defs{$d}) &&
+       defined($defs{$d}{IODev}) &&
+       $defs{$d}{IODev} == $hash){
+        Log3 $hash, 2, "deleting port for $d";
+        delete $defs{$d}{IODev};
+      }
+  }
+  DevIo_CloseDev($hash);
+  return undef;
+}
+sub StuderXcom232i_Notify(@) {##########################################################
+  my ($hash,$dev) = @_;
+  if (($dev->{NAME} eq "global" && grep (m/^INITIALIZED$|^REREADCFG$/,@{$dev->{CHANGED}}))&&($hash->{dummy}==0)){
+    DevIo_OpenDev($hash, 0, "StuderXcom232i_DoInit");
+  }
+  return;
+}
+
+sub StuderXcom232i_Reconnect($) {###########################################################
+  my($in ) = shift;
+  my(undef,$name) = split(':',$in);
+  my $hash = $defs{$name};
+  #Log 0,"StuderXcom232i_Reconnect";
+  RemoveInternalTimer( "reconnect:".$name);
+
+  if ($init_done==1) {
+    DevIo_OpenDev($hash, 0, "StuderXcom232i_DoInit") if(($hash->{STATE} eq "disconnected")&&($hash->{dummy}==0));
+  } else {
+    InternalTimer(gettimeofday()+10, "StuderXcom232i_Reconnect", "reconnect:".($name), 1);
+  }
+}
+  
+sub StuderXcom232i_Attr(@) {############################################################
+  my ($cmd,$name, $aName,$aVal) = @_;
+  
+  my $hash=$defs{$name};
+  
+  if($aName eq "dummy"){
+    if ($cmd eq "set" && $aVal != 0){
+      RemoveInternalTimer( "poll:".$name);
+      RemoveInternalTimer( "timeout:".$name);
+      DevIo_CloseDev($hash);
+      delete($hash->{WQUEUE});
+      delete($hash->{RQUEUE});
+      $hash->{STATE} = "ok";
+      $hash->{dummy}=1;
+    }
+    else{
+      $hash->{dummy}=0;
+      delete($hash->{WQUEUE});
+      delete($hash->{RQUEUE});
+      if ($init_done){
+        DevIo_OpenDev($hash, 1, "StuderXcom232i_DoInit");
+      }
+    }
+  }
+  return;
+}
+
+sub StuderXcom232i_Set($@) {############################################################
+  my ($hash, @a) = @_;
+  my $name = $hash->{NAME};
+
+  if( @a < 2 ) {
+    return( "at least one parameter is needed" ) ;
+  }
+
+  $name = shift( @a );
+  my $cmd = shift( @a );
+
+  if( $cmd eq "?" ) {
+    # this one should give us a drop down list
+    my $res = "Unknown argument ?, choose one of " . 
+              "read ";
+
+    return( $res );
+  } elsif( $cmd eq "read" ) {
+    StuderXcom232i_Write($hash,"101:".READ_PROPERTY.":1:".$a[0].":1:FLOAT:");
+  }
+
+  return( undef );
+}
+
+sub StuderXcom232i_Write($$) {#########################################################
+  my ($hash,$msg) = @_;
+
+  return unless ($init_done);
+  
+  my ($dst_addr,$service_id,$object_type,$object_id,$property_id,$format,$property_data)=split(":",$msg);
+
+  # build frame data
+  my $frame_data=pack "CCvVv",0x00,$service_id,$object_type,$object_id,$property_id;
+  if ($service_id == WRITE_PROPERTY) {
+    if (defined($format) && defined($property_data)) {
+      $frame_data.=pack "C",$property_data if($format eq 'BOOL');
+      $frame_data.=pack "f<",$property_data if($format eq 'FLOAT');
+      $frame_data.=pack "v",$property_data if($format eq 'ENUM');
+      $frame_data.=pack "V",$property_data if($format eq 'INT32');
+    }
+  }
+  
+  # build header
+  my $header=pack 'CVVv',0x00,1,$dst_addr,bytes::length($frame_data);
+
+  # add checksums
+  $frame_data.=pack 'v',StuderXcom232i_checksum($frame_data);
+  $header=(pack 'C',0xaa) . $header . pack 'v',StuderXcom232i_checksum($header);
+
+  if ($service_id == WRITE_PROPERTY) {
+    StuderXcom232i_LogFrame($hash,"AddWQueue",$header.$frame_data,5);
+    StuderXcom232i_AddWQueue($hash, $header.$frame_data);
+  }
+  if ($service_id == READ_PROPERTY) {
+    StuderXcom232i_LogFrame($hash,"AddRQueue",$header.$frame_data,5);
+    StuderXcom232i_AddRQueue($hash, $header.$frame_data);
+  }
+}
+
+sub StuderXcom232i_Read($) {############################################################
+# called from the global loop, when the select for hash->{FD} reports data
+  my ($hash) = @_;
+  my $buf = DevIo_SimpleRead($hash);
+  return "" if(!defined($buf));
+
+  my $name = $hash->{NAME};
+
+  my $pdata = $hash->{helper}{PARTIAL};
+  $pdata .= $buf;
+
+  StuderXcom232i_LogFrame($hash,"StuderXcom232i_Read",$pdata,4);
+  RemoveInternalTimer( "timeout:".$name);
+
+  # check received data
+  if(ord(bytes::substr($pdata, 0, 1)) == 0xaa) {
+    # header complete ?
+    if(bytes::length($pdata) >= 14) {
+      # header checksum ok ?
+      if(StuderXcom232i_checksum_is_ok(substr $pdata,1,13)) {
+        # unpack header
+        my (undef,$frame_flags,$src_addr,$dest_addr,$data_length,$checksum,$frame_data)=unpack "CCVVvva*",$pdata;
+        # frame complete ?
+        if(bytes::length($frame_data) >= $data_length+2) {
+          # frame_data checksum ok ?
+          if(StuderXcom232i_checksum_is_ok($frame_data)) {
+            # unpack frame_data
+            my ($flags,$service_id,$object_type,$object_id,$property_id,$value)=unpack "CCvVva*",(substr $frame_data,0,$data_length);
+            Dispatch($hash, "StuderXT:$src_addr:$service_id:$object_type:$object_id:$property_id:".join(":",unpack("C*", $value)), undef);
+            readingsSingleUpdate( $hash, "received", (unpack "H*",$pdata) , 1 );
+            $hash->{helper}{state}="idle";
+          } else {
+            Log3 $hash, 1,"StuderXcom232i_Read: bad frame_data checksum";
+            $hash->{helper}{PARTIAL} = undef;
+            $hash->{helper}{state}="idle";
+          }
+        } else {
+          $hash->{helper}{PARTIAL} = $pdata;
+          InternalTimer(gettimeofday()+AttrVal($name,"timeout",5), "StuderXcom232i_Timeout", "timeout:".$name, 1);
+        }
+      } else {
+        Log3 $hash, 1,"StuderXcom232i_Read: bad header checksum";
+        $hash->{helper}{PARTIAL} = undef;
+        $hash->{helper}{state}="idle";
+      }
+    } else {
+      $hash->{helper}{PARTIAL} = $pdata;
+      InternalTimer(gettimeofday()+AttrVal($name,"timeout",5), "StuderXcom232i_Timeout", "timeout:".$name, 1);
+    }
+  } else {
+    Log3 $hash, 1,"StuderXcom232i_Read: bad start_byte";
+    $hash->{helper}{PARTIAL} = undef;
+    $hash->{helper}{state}="idle";
+  }
+}
+
+sub StuderXcom232i_Parse($$) {##########################################################
+  my ($hash, $rmsg) = @_;
+  my $name = $hash->{NAME};
+
+  StuderXcom232i_LogFrame($hash,"StuderXcom232i_Parse",$rmsg,4);
+
+  if($hash->{helper}{state} eq "idle") {
+    return undef;
+  }
+
+  # Dispatch($hash, "ModbusCoil:$rx_hd_unit_id:".unpack("x2n", $rmsg).":$rx_bd_fc:1:".unpack("x4n", $rmsg), undef); 
+
+  RemoveInternalTimer( "timeout:".$name);
+  $hash->{helper}{state}="idle";
+}
+
+sub StuderXcom232i_Ready($) {###########################################################
+  my ($hash) = @_;
+  
+  if(($hash->{STATE} eq "disconnected")&&($hash->{dummy}==0)) {
+    RemoveInternalTimer( "reconnect:".$hash->{NAME});
+    return DevIo_OpenDev($hash, 1, "StuderXcom232i_DoInit")
+  }
+  
+  # This is relevant for windows/USB only
+  my $po = $hash->{USBDev};
+  my ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags);
+  if($po) {
+    ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $po->status;
+  }
+  return ($InBytes && $InBytes>0);
+}
+
+sub StuderXcom232i_SimpleWrite(@) {#####################################################
+  my ($hash, $msg) = @_;
+
+  return if(!$hash || AttrVal($hash->{NAME}, "dummy", undef));
+
+  my $name = $hash->{NAME};
+  if(($hash->{USBDev})||($hash->{DIODev})||($hash->{TCPDev})) {
+    $hash->{helper}{state}="active";
+    RemoveInternalTimer( "timeout:".$name);
+    InternalTimer(gettimeofday()+AttrVal($name,"timeout",5), "StuderXcom232i_Timeout", "timeout:".$name, 1);
+    StuderXcom232i_LogFrame($hash,"StuderXcom232i_SimpleWrite",$msg,5);
+    $hash->{USBDev}->write($msg)    if($hash->{USBDev});
+    syswrite($hash->{DIODev}, $msg) if($hash->{DIODev});
+    syswrite($hash->{TCPDev}, $msg) if($hash->{TCPDev});
+    $hash->{helper}{lastSimpleWrite}=unpack 'h*',$msg;
+
+    # Some linux installations are broken with 0.001, T01 returns no answer
+    select(undef, undef, undef, 0.01);
+  }
+}
+
+sub StuderXcom232i_DoInit($) {##########################################################
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+
+  #Log 0,"StuderXcom232i_DoInit";
+  # devio.pm does not support user defined settings, try to change here
+  if (defined($hash->{USBDev})) {
+    my $po=$hash->{USBDev};
+
+    $po->databits($hash->{helper}{databits});
+    $po->parity($hash->{helper}{parity});
+    $po->stopbits($hash->{helper}{stopbits});
+    $po->write_settings;
+  }
+  
+  $hash->{helper}{state}="idle";
+
+  return undef;
+}
+
+sub
+StuderXcom232i_Timeout($) ##################################################
+{
+  my($in ) = shift;
+  my(undef,$name) = split(':',$in);
+  my $hash = $defs{$name};
+
+  Log3 $hash, 3,"StuderXcom232i_Timeout";
+
+  $hash->{STATE} = "timeout";
+  $hash->{helper}{state}="idle";
+  $hash->{helper}{PARTIAL} = undef;
+}
+
+sub
+StuderXcom232i_SendFromWQueue($$) ##################################################
+{
+  my ($hash, $bstring) = @_;
+  my $name = $hash->{NAME};
+
+  if($bstring ne "") {
+    StuderXcom232i_SimpleWrite($hash, $bstring);
+  }
+  InternalTimer(gettimeofday()+0.02, "StuderXcom232i_HandleWriteQueue", $hash, 1);
+}
+
+sub
+StuderXcom232i_AddWQueue($$) ##################################################
+{
+  my ($hash, $bstring) = @_;
+  if(!$hash->{WQUEUE}) {
+    if($hash->{helper}{state} eq "idle") {
+      $hash->{WQUEUE} = [ $bstring ];
+      StuderXcom232i_SendFromWQueue($hash, $bstring);
+    } else {
+      $hash->{WQUEUE} = [ $bstring ];
+      push(@{$hash->{WQUEUE}}, $bstring);
+      InternalTimer(gettimeofday()+0.02, "StuderXcom232i_HandleWriteQueue", $hash, 1);
+    }
+  } else {
+    Log3 $hash, 5,"adding to WQUEUE - ".scalar(@{$hash->{WQUEUE}});
+    push(@{$hash->{WQUEUE}}, $bstring);
+  }
+}
+
+sub
+StuderXcom232i_HandleWriteQueue($) ##################################################
+{
+  my $hash = shift;
+  if($hash->{helper}{state} eq "idle") {
+    my $arr = $hash->{WQUEUE};
+    if(defined($arr) && @{$arr} > 0) {
+      shift(@{$arr});
+      if(@{$arr} == 0) {
+        delete($hash->{WQUEUE});
+        return;
+      }
+      Log3 $hash, 4,"WQUEUE: ".scalar(@{$arr});
+      my $bstring = $arr->[0];
+      if($bstring eq "") {
+        StuderXcom232i_HandleWriteQueue($hash);
+      } else {
+        StuderXcom232i_SendFromWQueue($hash, $bstring);
+      }
+    }
+  } else {
+    InternalTimer(gettimeofday()+0.02, "StuderXcom232i_HandleWriteQueue", $hash, 1);
+  }
+} 
+
+sub
+StuderXcom232i_SendFromRQueue($$) ##################################################
+{
+  my ($hash, $bstring) = @_;
+  my $name = $hash->{NAME};
+
+  if($bstring ne "") {
+    StuderXcom232i_SimpleWrite($hash, $bstring);
+  }
+  InternalTimer(gettimeofday()+0.02, "StuderXcom232i_HandleReadQueue", $hash, 1);
+}
+
+sub
+StuderXcom232i_AddRQueue($$) ##################################################
+{
+  my ($hash, $bstring) = @_;
+  if(!$hash->{RQUEUE}) {
+    if(($hash->{helper}{state} eq "idle")&&(!defined($hash->{WQUEUE}))) {
+      $hash->{RQUEUE} = [ $bstring ];
+      StuderXcom232i_SendFromRQueue($hash, $bstring);
+    } else {
+      $hash->{RQUEUE} = [ $bstring ];
+      push(@{$hash->{RQUEUE}}, $bstring);
+      InternalTimer(gettimeofday()+0.02, "StuderXcom232i_HandleReadQueue", $hash, 1);
+    }
+  } else {
+    my $add=1;
+    for my $el (@{$hash->{RQUEUE}}) {
+        if($el eq $bstring) {
+            $add=0;
+        }
+    }
+    if ($add==1) {
+        Log3 $hash, 5,"adding to RQUEUE - ".scalar(@{$hash->{RQUEUE}});
+        push(@{$hash->{RQUEUE}}, $bstring);
+    }
+  }
+}
+
+sub
+StuderXcom232i_HandleReadQueue($) ##################################################
+{
+  my $hash = shift;
+  if(($hash->{helper}{state} eq "idle")&&(!defined($hash->{WQUEUE}))) {
+    my $arr = $hash->{RQUEUE};
+    if(defined($arr) && @{$arr} > 0) {
+      shift(@{$arr});
+      if(@{$arr} == 0) {
+        delete($hash->{RQUEUE});
+        return;
+      }
+      Log3 $hash, 4,"RQUEUE: ".scalar(@{$arr});
+      my $bstring = $arr->[0];
+      if($bstring eq "") {
+        StuderXcom232i_HandleReadQueue($hash);
+      } else {
+        StuderXcom232i_SendFromRQueue($hash, $bstring);
+      }
+    }
+  } else {
+    InternalTimer(gettimeofday()+0.02, "StuderXcom232i_HandleReadQueue", $hash, 1);
+  }
+} 
+
+sub StuderXcom232i_LogFrame($$$$) {
+  my ($hash,$c,$data,$verbose)=@_;
+
+  my @dump = map {sprintf "%02X", $_ } unpack("C*", $data);
+
+  $hash->{helper}{lastFrame}=$c." ".join(" ",@dump) if($c eq 'SimpleWrite');
+
+  Log3 $hash, $verbose,"StuderXcom232i_LogFrame: ".$c." ".join(" ",@dump);
+}
+
+# SCOM checksum
+# A = 0xFF
+# B = 0
+# For I FROM 0 TO number_of_bytes -1 DO
+#   A := (A + DATA[I]) mod 0x100;
+#   B := (B + A) mod 0x100;
+# END
+# checksum[0] := A
+# checksum[1] := B
+
+sub StuderXcom232i_checksum($) {
+  my ($frame) =@_;
+  my $a = 0xFF;
+  my $b = 0;
+  my $chr;
+
+  for my $i (0..bytes::length($frame)-1) {
+    $chr = ord(bytes::substr($frame, $i, 1));
+    $a = ($a + $chr) % 0x100;
+    $b = ($b + $a) % 0x100;
+  }
+  return $a + ($b << 8);
+}
+
+# Check the checksum of an SCOM frame.
+#   return true if checksum is ok
+sub StuderXcom232i_checksum_is_ok($) {
+  my ($frame) = @_;
+  my $crc = unpack('v', bytes::substr($frame, -2));
+  return ($crc == StuderXcom232i_checksum(bytes::substr($frame,0,-2)));
+}
+
+1;
+
+=pod
+=begin html
+
+<a name="StuderXcom232i"></a>
+<h3>StuderXcom232i</h3>
+<ul>
+Todo
+</ul>
+
+=end html
+=cut
---
+++ 37_StuderXT.pm
@@ -0,0 +1,353 @@
+﻿# $Id: 37_StuderXT.pm 0004 $
+# 150417 0001 initial release
+# 150510 0002 added warning if config file can not be found
+# 150515 0003 added write support, autostart readcycle
+# 150516 0004 write times for grid feeding
+# TODO:
+
+package main;
+
+use strict;
+use warnings;
+use SetExtensions;
+use Time::HiRes qw(gettimeofday time);
+
+sub StuderXT_Parse($$);
+
+sub
+StuderXT_Initialize($)
+{
+  my ($hash) = @_;
+
+  $hash->{Match}     = "^StuderXT";
+  $hash->{DefFn}     = "StuderXT_Define";
+  $hash->{UndefFn}   = "StuderXT_Undef";
+  $hash->{SetFn}     = "StuderXT_Set"; 
+  $hash->{NotifyFn}  = "StuderXT_Notify";
+  #$hash->{FingerprintFn}   = "StuderXT_Fingerprint";
+  $hash->{ParseFn}   = "StuderXT_Parse";
+  $hash->{AttrFn}    = "StuderXT_Attr";
+  $hash->{AttrList}  = "IODev ".
+                       "$readingFnAttributes";
+}
+
+sub
+StuderXT_Define($$)
+{
+  my ($hash, $def) = @_;
+  my @a = split("[ \t][ \t]*", $def);
+
+  if(@a != 4) {
+    my $msg = "wrong syntax: define <name> StuderXT <addr> <config file>";
+    Log3 undef, 2, $msg;
+    return $msg;
+  }
+
+  if(!StuderXT_is_integer($a[2])) {
+    return "$a[2] is not a valid address";
+  }
+
+  if(($a[2]<101)||($a[2]>109)) {
+    return "address must be between 101 and 109";
+  }
+
+  unless (-e $a[3]) {
+    unless (-e "$attr{global}{modpath}/FHEM/".$a[3]) {
+      Log3 $a[0], 1, "StuderXT_Define: $a[0]: config file $a[3] not found";
+      return "config file not found";
+    }
+    $a[3]="$attr{global}{modpath}/FHEM/".$a[3];
+  }
+  
+  $hash->{CONFIGFILE}=$a[3];
+  my $name = $a[0];
+
+  $hash->{helper}{addr}=$a[2];
+  if(defined($hash->{helper}{addr})&&defined($modules{StuderXT}{defptr}{$hash->{helper}{addr}}{$name})) {
+    Log3 $name, 5, "Removing $hash->{helper}{addr} $name";
+    delete( $modules{StuderXT}{defptr}{$hash->{helper}{addr}}{$name} );
+  }
+
+  Log3 $name, 5, "Def $hash->{helper}{addr} $name";
+  $modules{StuderXT}{defptr}{$hash->{helper}{addr}}{$name} = $hash;
+  
+  if(defined($attr{$name}{IODev})) {
+    $hash->{IODev}->{NAME}=$attr{$name}{IODev};
+  } else {
+    AssignIoPort($hash);
+  }
+  if(defined($hash->{IODev}->{NAME})) {
+    Log3 $name, 3, "$name: I/O device is " . $hash->{IODev}->{NAME};
+  } else {
+    Log3 $name, 1, "$name: no I/O device";
+  }
+
+  StuderXT_ReadConfig($hash);
+  StuderXT_ReadCycle($hash) if ($init_done>0);
+
+  return undef;
+}
+
+#####################################
+sub
+StuderXT_ReadConfig($)
+{
+  my ($hash) = @_;
+
+  delete($hash->{helper}{readings}) if defined($hash->{helper}{readings});
+  
+  open(my $data, '<', $hash->{CONFIGFILE});
+  while (my $line = <$data>) {
+    chomp $line;
+    my @fields = split /[,;]/ , $line;
+    if (@fields>=6) {
+      if($fields[0] eq '1') {
+        $hash->{helper}{readings}{$fields[1]}{name}=$fields[2];
+        $hash->{helper}{readings}{$fields[1]}{unit}=$fields[3];
+        $hash->{helper}{readings}{$fields[1]}{format}=$fields[4];
+        $hash->{helper}{readings}{$fields[1]}{objectType}=$fields[5] eq 'RW'?2:1;
+        $hash->{helper}{readings}{$fields[1]}{propertyId}=$fields[5] eq 'RW'?2:1;
+        $hash->{helper}{readings}{$fields[1]}{readInterval}=$fields[6];
+        $hash->{helper}{readings}{$fields[1]}{lastRead}=0;
+        $hash->{helper}{readings}{$fields[1]}{nextRead}=$fields[6];
+        for (my $i=7;$i<@fields;$i++) {
+          my @en=split(':',$fields[$i]);
+          if (@en==2) {
+            $hash->{helper}{readings}{$fields[1]}{enumVals}{$en[0]}=$en[1];          
+          }
+        }
+      }
+    }
+  }
+}
+
+#####################################
+sub
+StuderXT_tcb_ReadCycle($)
+{
+    my($in ) = shift;
+    my(undef,$name) = split(':',$in);
+    my $hash = $defs{$name};
+
+    StuderXT_ReadCycle($hash);
+}
+
+#####################################
+sub
+StuderXT_ReadCycle($)
+{
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+
+  RemoveInternalTimer( "ReadCycle:$name");
+  if(defined($hash->{helper}{readings}) && defined($hash->{IODev}->{NAME})) {
+    foreach my $r ( keys %{$hash->{helper}{readings}} ) {
+      if(($hash->{helper}{readings}{$r}{nextRead}<time()) && ($hash->{helper}{readings}{$r}{nextRead}!=0)) {
+        IOWrite($hash,join(':',$hash->{helper}{addr},'1',$hash->{helper}{readings}{$r}{objectType},$r,$hash->{helper}{readings}{$r}{propertyId},$hash->{helper}{readings}{$r}{format},''));
+      }
+    }
+  }
+  InternalTimer( gettimeofday() + 1, 
+     "StuderXT_tcb_ReadCycle",
+     "ReadCycle:$name", 
+     0 );
+}
+
+#####################################
+sub
+StuderXT_Undef($$)
+{
+  my ($hash, $arg) = @_;
+  my $name = $hash->{NAME};
+  my $addr = $hash->{helper}{addr};
+
+  Log3 $name, 5, "Undef $addr $name";
+  RemoveInternalTimer( "ReadCycle:$name");
+  delete( $modules{StuderXT}{defptr}{$addr}{$name} );
+
+  return undef;
+}
+
+#####################################
+sub
+StuderXT_Set($@)
+{
+  my ($hash, @a) = @_; 
+  my $ret = undef;
+  my $na = int(@a); 
+  my $name = $hash->{NAME};
+  
+  return "no set value specified" if($na==0);
+
+  if($a[1] eq '?')
+  {
+    my $list = "writeToFlash:on,off gridFeeding:on,off charger:on,off chargeCurrent gridFeedingCurrent"; 
+    return $list; 
+  }
+
+  if( @a != 3 ) {
+      my $msg = "StuderXT_Set: no arguments for $a[1] given.";
+      Log3( $hash, 3, $msg );
+      return( $msg );
+  }
+
+#  my ($dst_addr,$service_id,$object_type,$object_id,$property_id,$format,$property_data)=split(":",$msg);
+  
+  if($a[1] eq 'writeToFlash') {
+    if($a[2] eq 'on') {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1550','5','BOOL','1'));
+    } elsif ($a[2] eq 'off') {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1550','5','BOOL','0'));
+    }
+  }
+
+  if($a[1] eq 'gridFeeding') {
+    if($a[2] eq 'on') {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1525','5','INT32',1));
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1526','5','INT32',23*60+59));
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1127','5','BOOL','1'));
+    } elsif ($a[2] eq 'off') {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1127','5','BOOL','0'));
+    }
+  }
+
+  if($a[1] eq 'charger') {
+    if($a[2] eq 'on') {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1125','5','BOOL','1'));
+    } elsif ($a[2] eq 'off') {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1125','5','BOOL','0'));
+    }
+  }
+
+  if($a[1] eq 'chargeCurrent') {
+    if(StuderXT_is_float($a[2])) {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1138','5','FLOAT',$a[2]));
+      IOWrite($hash,join(':',$hash->{helper}{addr},'1','1','3004','1','FLOAT',''));
+    } else {
+      my $msg = "StuderXT_Set: invalid value $a[1] given.";
+      Log3( $hash, 3, $msg );
+      return( $msg );
+    }
+  }
+
+  if($a[1] eq 'gridFeedingCurrent') {
+    if(StuderXT_is_float($a[2])) {
+      IOWrite($hash,join(':',$hash->{helper}{addr},'2','2','1523','5','FLOAT',$a[2]));
+    } else {
+      my $msg = "StuderXT_Set: invalid value $a[1] given.";
+      Log3( $hash, 3, $msg );
+      return( $msg );
+    }
+  }
+}
+
+#####################################
+sub
+StuderXT_Get($@)
+{
+  my ($hash, $name, $cmd, @args) = @_;
+
+  return "\"get $name\" needs at least one parameter" if(@_ < 3);
+
+  my $list = "";
+
+  return "Unknown argument $cmd, choose one of $list";
+}
+
+sub
+StuderXT_Fingerprint($$)
+{
+  my ($name, $msg) = @_;
+  return ( "", $msg );
+}
+
+sub
+StuderXT_Parse($$)
+{
+  my ($hash, $msg) = @_;
+  my $name = $hash->{NAME};
+
+  #Dispatch($hash, "StuderXT:$src_addr:$service_id:$object_type:$object_id:$property_id".join(":",unpack("C*", $value)), undef);
+
+  my (undef,$src_addr,$service_id,$object_type,$object_id,$property_id,@vals) = split(":",$msg);
+
+  my @list;
+
+  my $rhash = $modules{StuderXT}{defptr}{$src_addr};
+  if($rhash) {
+    foreach my $n (keys %{$rhash}) {
+      my $lh = $rhash->{$n};
+      if((defined($lh->{IODev})) && ($lh->{IODev} == $hash)) {
+        $n = $lh->{NAME};
+        $lh->{StuderXT_lastRcv} = TimeNow();
+        if(defined($lh->{helper}{readings}{$object_id})) {
+          my $v=0;
+          $v=unpack "f", pack "C4", @vals if($lh->{helper}{readings}{$object_id}{format} eq 'FLOAT');
+          $v=$vals[0] if($lh->{helper}{readings}{$object_id}{format} eq 'BOOL');
+          $v=unpack "v", pack "C2", @vals if($lh->{helper}{readings}{$object_id}{format} eq 'ENUM');
+          $v=unpack "V", pack "C4", @vals if($lh->{helper}{readings}{$object_id}{format} eq 'INT32');
+          readingsBeginUpdate($lh);
+          readingsBulkUpdate($lh,$lh->{helper}{readings}{$object_id}{name},$v);
+          readingsEndUpdate($lh,1);
+          $lh->{helper}{readings}{$object_id}{lastRead}=time();
+          if($lh->{helper}{readings}{$object_id}{readInterval}>0) {
+            $lh->{helper}{readings}{$object_id}{nextRead}=time()+$lh->{helper}{readings}{$object_id}{readInterval};
+          } else {
+            $lh->{helper}{readings}{$object_id}{nextRead}=0;
+          }
+        }
+        push(@list, $n); 
+      }
+    }
+  } else {
+   Log3 $name, 2, "StuderXT_Parse: invalid address $src_addr";
+   #return "UNDEFINED StuderXT_$rname StuderXT $src_addr";
+   return undef;
+  }
+  return @list;
+}
+
+sub
+StuderXT_Attr(@)
+{
+  my ($cmd, $name, $attrName, $attrVal) = @_;
+  my $hash=$defs{$name};
+
+  return undef;
+}
+
+sub StuderXT_Notify(@) {##########################################################
+  my ($hash,$dev) = @_;
+  my $name = $hash->{NAME}; # own name / hash
+  my $devName = $dev->{NAME}; # Device that created the events
+
+  if ($devName eq "global" && grep (m/^INITIALIZED$|^REREADCFG$/,@{$dev->{CHANGED}})){
+    StuderXT_ReadCycle($hash);
+  }
+
+  # event from IODev
+  if (defined($hash->{IODev}) && defined($hash->{IODev}->{NAME}) && ($devName eq $hash->{IODev}->{NAME})) {
+  }
+  return;
+}
+
+sub StuderXT_is_integer {
+   defined $_[0] && $_[0] =~ /^[+-]?\d+$/;
+}
+
+sub StuderXT_is_float {
+   defined $_[0] && $_[0] =~ /^[+-]?\d+(\.\d+)?$/;
+}
+
+1;
+
+=pod
+=begin html
+
+<a name="StuderXT"></a>
+<h3>StuderXT</h3>
+<ul>
+</ul>
+
+=end html
+=cut
